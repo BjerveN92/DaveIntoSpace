869949fe0ba4b6d2a36489b5bbae73f178257a8a
Martin Bjervig <martinbjervig@gmail.com>
2024-11-08 11:52:50 +0100

    added subclasses

--- src/Character.java
+++ src/Character.java
@@ -3,14 +3,14 @@ public abstract class Character {
     protected String name;
     protected int health;  
     protected int armor;
-    protected boolean isArmed;
     
     
-    public Character(String name, int health, int armor, boolean isArmed) {
+    
+    public Character(String name, int health, int armor) {
         this.name = name;
         this.health = health;
         this.health = armor;
-        this.isArmed = isArmed;
+        
     }
 
     public abstract void attack();
