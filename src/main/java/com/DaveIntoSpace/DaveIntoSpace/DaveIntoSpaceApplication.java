ec946dfcdd8786358f9469ba6bdff39163f3d4a6
Martin Bjervig <martinbjervig@gmail.com>
2025-04-18 11:16:34 +0200

    changed the whole entire project into SpringBoot+MongoDB, time to have some fungit status

---
+++ src/main/java/com/DaveIntoSpace/DaveIntoSpace/DaveIntoSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.DaveIntoSpace.DaveIntoSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaveIntoSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaveIntoSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DaveIntoSpace/DaveIntoSpace/DaveIntoSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DaveIntoSpace.DaveIntoSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaveIntoSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
