0c2b628002c6f2c70fc7db16294fb6c2f57bc499
Martin Bjervig <martinbjervig@gmail.com>
2025-04-21 14:03:08 +0200

    added constructor + getters&setters

--- src/main/java/com/DaveIntoSpace/DaveIntoSpace/models/Character.java
+++ src/main/java/com/DaveIntoSpace/DaveIntoSpace/models/Character.java
@@ -27,4 +27,89 @@ public class Character {
     private SecondaryWeapons secondaryWeapons;
     private ExplosiveWeapons explosiveWeapons;
 
+    public Character(String id, String name, int level, Species species, Faction faction, LightWeapons lightWeapons,
+            PrimaryWeapons primaryWeapons, SecondaryWeapons secondaryWeapons, ExplosiveWeapons explosiveWeapons) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.species = species;
+        this.faction = faction;
+        this.lightWeapons = lightWeapons;
+        this.primaryWeapons = primaryWeapons;
+        this.secondaryWeapons = secondaryWeapons;
+        this.explosiveWeapons = explosiveWeapons;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public Faction getFaction() {
+        return faction;
+    }
+
+    public void setFaction(Faction faction) {
+        this.faction = faction;
+    }
+
+    public LightWeapons getLightWeapons() {
+        return lightWeapons;
+    }
+
+    public void setLightWeapons(LightWeapons lightWeapons) {
+        this.lightWeapons = lightWeapons;
+    }
+
+    public PrimaryWeapons getPrimaryWeapons() {
+        return primaryWeapons;
+    }
+
+    public void setPrimaryWeapons(PrimaryWeapons primaryWeapons) {
+        this.primaryWeapons = primaryWeapons;
+    }
+
+    public SecondaryWeapons getSecondaryWeapons() {
+        return secondaryWeapons;
+    }
+
+    public void setSecondaryWeapons(SecondaryWeapons secondaryWeapons) {
+        this.secondaryWeapons = secondaryWeapons;
+    }
+
+    public ExplosiveWeapons getExplosiveWeapons() {
+        return explosiveWeapons;
+    }
+
+    public void setExplosiveWeapons(ExplosiveWeapons explosiveWeapons) {
+        this.explosiveWeapons = explosiveWeapons;
+    }
+
 }
